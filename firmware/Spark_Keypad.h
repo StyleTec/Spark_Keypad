692261ab88c62b421c927540bef1db6e16eefc12
StyleTec <bm@flyinghero.de>
2014-12-10 21:05:45 +0100

    directory changes

--- firmware/Spark_Keypad.h
+++ firmware/Spark_Keypad.h
@@ -33,7 +33,7 @@
 #ifndef KEYPAD_H
 #define KEYPAD_H
 
-#include "utility/Key.h"
+#include "Key.h"
 
 // bperrybap - Thanks for a well reasoned argument and the following macro(s).
 // See http://arduino.cc/forum/index.php/topic,142041.msg1069480.html#msg1069480
